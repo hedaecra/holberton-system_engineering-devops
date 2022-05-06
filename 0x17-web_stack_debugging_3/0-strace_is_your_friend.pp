c2c920d7fbc0cf3385d03768d6065d952f3fd0a8
hedaecra <3883@holbertonschool.com>
2022-05-05 21:53:57 -0500

    Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,12 @@
+# syntax error in wp-settings file
+exec { 'fix_syntax' :
+  path     => '/usr/bin:/usr/sbin:/bin',
+  provider => 'shell',
+  command  => 'sudo sed -i s/class-wp-locale.phpp/class-wp-locale.php/g /var/www/html/wp-settings.php',
+}
+
+exec { 'reload' :
+  path     => '/usr/bin:/usr/sbin:/bin',
+  provider => 'shell',
+  command  => 'sudo /etc/init.d/apache2 restart'
+}
