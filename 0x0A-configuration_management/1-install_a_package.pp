5dab2a3de732e3a63fa7e89319040a2acb3d15ed
hedaecra <3883@holbertonschool.com>
2022-05-20 21:40:29 -0500

    Update File

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # installs the package puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
+package { 'puppet-lint' :
+  ensure   => '2.1.1',
   provider => 'gem',
-  source   => 'https://rubygems.org',
 }
